efd9fc32c61dd973f5fa061678e48ee15ea57087
Kadenj117 <kadenj117@gmail.com>
2021-07-15 15:40:05 -0600

    fixed eslint

--- cachiql-demo/webpack.config.js
+++ cachiql-demo/webpack.config.js
@@ -1,9 +1,3 @@
-/* eslint-disable no-undef */
-/* eslint-disable */
-//import webpack from 'webpack';
-// import path from 'path';
-// import HtmlWebpackPlugin from 'html-webpack-plugin';
-
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
