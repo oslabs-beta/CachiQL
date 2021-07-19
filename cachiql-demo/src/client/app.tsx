0abf2a62878e349e8a369e01fbc54a7d5913d37b
Vanessa Lutz <vanessalutz@vanessas-mbp.lan>
2021-07-19 09:35:24 -0700

    added barcharts

--- cachiql-demo/src/client/app.tsx
+++ cachiql-demo/src/client/app.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FloatNavigationMenuStyle } from './Components/Navigation'
 import { Graphiql } from './Components/GraphiQL'
+import { BuildBarChart } from './Components/BarChart';
 import CachiQLLogo from '../../dist/assets/cachiql_(1).svg'
 
 
@@ -9,6 +10,7 @@ const App = () => (
     <h1>Welcome to CachiQL</h1>
     <FloatNavigationMenuStyle />
     <Graphiql />
+    <BuildBarChart />
   </div>
 );
 export default App;
