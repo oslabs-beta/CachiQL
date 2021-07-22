143d300280dfec119f9311a725773e9297f42e7f
Kadenj117 <kadenj117@gmail.com>
2021-07-22 10:08:44 -0600

    fixed backend graphiQL IDE

--- cachiql-demo/src/server/server.js
+++ cachiql-demo/src/server/server.js
@@ -1,19 +1,151 @@
-/* eslint-disable */
+/*eslint-disable*/
+
 const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
-const graphqlSchema = require('./schema');
-const graphqlResolvers = require('./resolvers');
 const mongoose = require('mongoose');
+const { graphqlHTTP } = require('express-graphql');
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList,
+  GraphQLInt,
+  GraphQLNonNull,
+  GraphQLID
+} = require('graphql');
+const Author = require('./models/author');
+const Book = require('./models/book');
+const app = express();
 
-var app = express();
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  description: 'This represents an author of a book',
+  fields: () => ({
+    _id: { type: GraphQLNonNull(GraphQLID) },
+    firstName: { type: GraphQLNonNull(GraphQLString) },
+    lastName: { type: GraphQLNonNull(GraphQLString) },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve: async (author) => {
+        let fetched = await Book.find({ Author: author._id })
+        //console.log(fetched)
+        return fetched;
+      }
+    }
+  })
+})
 
-// The root provides a resolver function for each API endpoint
+const BookType = new GraphQLObjectType({
+  name: 'Book',
+  description: 'This represents a book written by an author',
+  fields: () => ({
+    _id: { type: GraphQLNonNull(GraphQLID) },
+    title: { type: GraphQLNonNull(GraphQLString) },
+    Author: {
+      type: AuthorType,
+      //need to change this to match db requirements
+      resolve: async (book) => {
+        let fetched = await Author.findOne({ books: book._id })
+        console.log(fetched)
+        return fetched;
+      }
+    }
+  })
+})
+
+const RootQueryType = new GraphQLObjectType({
+  name: 'Query',
+  description: 'Root Query',
+  fields: () => ({
+    book: {
+      type: BookType,
+      description: 'A single Book',
+      args: {
+        id: { type: GraphQLID }
+      },
+      //db query instead to get this
+      resolve: async (parent, args) => {
+        let fetched = await Book.findById(args.id)
+        return fetched;
+      }
+    },
+    books: {
+      type: new GraphQLList(BookType),
+      description: 'List of Books',
+      //query db in resolve instead of returning the books object
+      resolve: async () => {
+        let fetched = await Book.find({});
+        return fetched;
+      }
+    },
+    author: {
+      type: AuthorType,
+      description: 'A Single Author',
+      args: {
+        id: { type: GraphQLID }
+      },
+      //query db in resolve instead of returning the books object
+      resolve: async (parent, args) => {
+        return Author.findOne({ _id: args.id })
+      }
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      description: 'List of all Authors',
+      //query db in resolve instead of returning the books object
+      resolve: async () => {
+        let fetched = await Author.find({});
+        return fetched;
+      }
+    }
+  })
+});
+
+// const RootMutationType = new GraphQLObjectType({
+//   name: 'Mutations',
+//   description: 'Root Mutation',
+//   fields: () => ({
+//     addBook: {
+//       type: BookType,
+//       description: 'Add a Book',
+//       args: {
+//         name: { type: GraphQLNonNull(GraphQLString) },
+//         authorid: { type: GraphQLNonNull(GraphQLInt) }
+//       },
+//       resolve: async (parent, args) => {
+//         const statement = 'insert into books (name, authorid) values ($1, $2) returning *';
+//         const values = [args.name, args.authorid];
+//         const data = await db.query(statement, values);
+//         return data.rows[0];
+//       }
+//     },
+//     addAuthor: {
+//       type: AuthorType,
+//       description: 'Add an Author',
+//       args: {
+//         name: { type: GraphQLNonNull(GraphQLString) }
+//       },
+//       //change this to db query
+//       resolve: async (parent, args) => {
+//         // const author = { id: authors.length + 1, name: args.name };
+//         // authors.push(author);
+//         // return author;
+//         const statement = 'insert into authors (name) values ($1) returning *';
+//         const values = [args.name];
+//         const data = await db.query(statement, values);
+//         return data.rows[0];
+//       }
+//     }
+//   })
+// })
+
+const schema = new GraphQLSchema({
+  query: RootQueryType,
+})
 
 app.use('/graphql', graphqlHTTP({
-  schema: graphqlSchema,
-  rootValue: graphqlResolvers,
-  graphiql: true,
-}));
+  schema: schema,
+  graphiql: true
+}))
 
 const uri = 'mongodb+srv://cachiql:cache@cachiql.pypfo.mongodb.net/cachiql?retryWrites=true&w=majority';
 const options = { useNewUrlParser: true, useUnifiedTopology: true }
@@ -22,6 +154,3 @@ mongoose.connect(uri, options)
   .catch(error => {
     throw error;
   });
-
-// app.listen(3000);
-// console.log('Running a GraphQL API server at http://localhost:3000/graphql');
