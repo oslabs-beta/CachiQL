af13de48f036c4a41a7ef21de0a021948226c804
Kadenj117 <kadenj117@gmail.com>
2021-07-23 11:07:45 -0600

    collecting visualizer data

--- cachiql-demo/src/client/Components/GraphiQL.tsx
+++ cachiql-demo/src/client/Components/GraphiQL.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GraphiQL from 'graphiql';
 import styled from 'styled-components';
 import 'graphiql/graphiql.min.css';
@@ -13,10 +13,38 @@ const Stylegraphiql = styled.div`
 `;
 
 export const Graphiql = () => {
+  const [loaded, setLoaded] = useState(false);
+  const [count, setCount] = useState([]);
+  const dataLoaded = () => {
+    fetch('./counter')
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        if (data > 0) {
+          console.log('loaded')
+          setCount(data || []);
+          setLoaded(true);
+        }
+        if (loaded) {
+          console.log('count edited')
+          console.log('loaded data', data)
+          setCount(data || []);
+          setLoaded(false);
+        }
+      })
+    console.log('data is loaded');
+    console.log('count', count);
+  }
+
+  const handleAdd = (event) => {
+    console.log('what is the event', event)
+    setCount(event || [])
+  }
   return (
     <Stylegraphiql>
       <div className="graphiql">
         <GraphiQL
+          ExecuteButton._onClick=
           fetcher={async (graphQLParams) => {
             const data = await fetch('graphql', {
               method: 'POST',
@@ -27,8 +55,10 @@ export const Graphiql = () => {
               body: JSON.stringify(graphQLParams),
               credentials: 'same-origin'
             });
+            dataLoaded();
             return data.json().catch(() => data.text());
           }}
+          ExecuteButton
         />
       </div>
     </Stylegraphiql>
