c8e57d591c19193ee1c483f6b8c35fdd1ee37a76
zapata124 <ecz001@live.com>
2022-10-11 21:24:41 -0400

    Created folder for avatar with index.ts for export

--- cachiql-demo/src/client/Components/Avatar/Avatars.tsx
+++ cachiql-demo/src/client/Components/Avatar/Avatars.tsx
@@ -10,10 +10,10 @@ const teamMembers: MemberType[] = [
   { name: 'Kaden Johnson', image: 'kadenPic.jpg' },
   { name: 'Vanessa Lutz', image: 'vanessaPic.jpg' },
   { name: 'Eddyddddd Zapata', image: 'eddyPic.jpg' },
-  { name: 'Fahad Shaikh', image: 'fahadPic.jpg' }
+  { name: 'Fahad Shaikh', image: 'fahadPic.jpg' },
 ];
 
-export const Avatars = () => {
+const Avatars: React.FC = () => {
   return (
     <React.Fragment>
       {teamMembers.map((member: MemberType) => {
@@ -22,3 +22,5 @@ export const Avatars = () => {
     </React.Fragment>
   );
 };
+
+export default Avatars;
