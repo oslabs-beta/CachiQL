b10faae06acbb1554650cdda9c58531378747ced
zapata124 <ecz001@live.com>
2022-10-30 17:59:01 -0400

    Changing installatinons

--- cachiql-demo/src/client/Components/AboutUs/AboutUs.tsx
+++ cachiql-demo/src/client/Components/AboutUs/AboutUs.tsx
@@ -2,11 +2,10 @@ import React, { useState } from 'react';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import Card from '@material-ui/core/Card';
-import Container from '@material-ui/core/Container';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import TeamCachiql from '../../assets/teamCachiql.svg';
-import { Box, Grid } from '@material-ui/core';
+import { Container, Box, Grid } from '@mui/material';
 import { useStyles } from './styles';
 import { teamMembers } from './members';
 
